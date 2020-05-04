927df4725d8b46194ed86b78974ad224e704fe30
ericksilva-dev <erick.kaique.silva272@gmail.com>
2020-05-03 23:50:39 -0300

    Implementando casos de validação para o Service de Lançamento

--- src/main/java/com/ericksilva/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/ericksilva/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -1,5 +1,93 @@
 package com.ericksilva.minhasfinancas.service.impl;
 
-public class LancamentoServiceImpl {
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
+
+import com.ericksilva.minhasfinancas.exceptions.RegraNegocioException;
+import com.ericksilva.minhasfinancas.model.entity.Lancamento;
+import com.ericksilva.minhasfinancas.model.entity.StatusLancamento;
+import com.ericksilva.minhasfinancas.model.repository.LancamentoRepository;
+import com.ericksilva.minhasfinancas.service.LancamentoService;
+
+public class LancamentoServiceImpl implements LancamentoService {
+	
+	private LancamentoRepository repository;
+	
+	public LancamentoServiceImpl(LancamentoRepository repository) {
+		this.repository = repository;
+	}
+	
+	@Override
+	@Transactional
+	public Lancamento salvar(Lancamento lancamento) {
+		validar(lancamento);
+		lancamento.setLancamento(StatusLancamento.PENDENTE);
+		return repository.save(lancamento);
+	}
+
+	@Override
+	@Transactional
+	public Lancamento atualizar(Lancamento lancamento) {
+		Objects.requireNonNull(lancamento.getId());
+		validar(lancamento);
+		return repository.save(lancamento);
+	}
+
+	@Override
+	public void deletar(Lancamento lancamento) {
+		Objects.requireNonNull(lancamento.getId());
+		repository.delete(lancamento);
+	}
+
+	@Override
+	@Transactional
+	public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
+		Example example = Example.of(lancamentoFiltro,
+				ExampleMatcher.matching()
+				.withIgnoreCase()
+				.withStringMatcher(StringMatcher.CONTAINING));
+		
+		return repository.findAll(example);
+	}
+
+	@Override
+	public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
+		lancamento.setLancamento(status);
+		atualizar(lancamento);
+	}
+
+	@Override
+	public void validar(Lancamento lancamento) {
+		if(lancamento.getDescricao() == null || lancamento.getDescricao().trim().equals("")) {
+			throw new RegraNegocioException("Informe uma descrição válida");
+		}
+		
+		if(lancamento.getMes() == null || lancamento.getMes() < 1 || lancamento.getMes() > 12) {
+			throw new RegraNegocioException("Informe um Mês válido");
+		}
+		
+		if(lancamento.getAno() == null || lancamento.getAno().toString().length() != 4) {
+			throw new RegraNegocioException("Informe um Ano Válido");
+		}
+		
+		if(lancamento.getUsuario() == null || lancamento.getUsuario().getId() == null) {
+			throw new RegraNegocioException("Informe um usuário válido");
+		}
+		
+		if(lancamento.getValor() == null || lancamento.getValor().compareTo(BigDecimal.ZERO) < 1) {
+			throw new RegraNegocioException("Informe um valor válido");
+		}
+		
+		if(lancamento.getTipo() == null) {
+			throw new RegraNegocioException("Informe um tipo de Lançamento");
+		}
+	}
 
 }
