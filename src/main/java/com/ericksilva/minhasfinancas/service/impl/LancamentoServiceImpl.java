1419626cf200050b64dd781e22c5144bf70753f1
ericksilva-dev <erick.kaique.silva272@gmail.com>
2020-05-05 23:33:05 -0300

    Criando lógica de obtenção de saldo por id do usuário, e implementando lógicas necessárias

--- src/main/java/com/ericksilva/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/ericksilva/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -5,16 +5,16 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ericksilva.minhasfinancas.exceptions.RegraNegocioException;
 import com.ericksilva.minhasfinancas.model.entity.Lancamento;
 import com.ericksilva.minhasfinancas.model.entity.StatusLancamento;
+import com.ericksilva.minhasfinancas.model.entity.TipoLancamento;
 import com.ericksilva.minhasfinancas.model.repository.LancamentoRepository;
 import com.ericksilva.minhasfinancas.service.LancamentoService;
 
@@ -96,6 +96,23 @@ public class LancamentoServiceImpl implements LancamentoService {
 	@Override
 	public Optional<Lancamento> obterPorId(Long id) {
 		return repository.findById(id);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public BigDecimal obterSaldoPorUsuario(Long id) {
+		BigDecimal receitas = repository.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.RECEITA.name());
+		BigDecimal despesas = repository.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.DESPESA.name());
+		
+		if(receitas == null) {
+			receitas = BigDecimal.ZERO;
+		}
+		
+		if(despesas == null) {
+			despesas = BigDecimal.ZERO;
+		}
+		
+		return receitas.subtract(despesas);
 	}
 
 }
