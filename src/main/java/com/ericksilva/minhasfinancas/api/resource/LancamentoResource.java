9623f6345dd960441461eab6654f29c4f4fe2582
ericksilva-dev <erick.kaique.silva272@gmail.com>
2020-05-05 23:13:20 -0300

    Criando método que irá atualizar o status dos lançamento

--- src/main/java/com/ericksilva/minhasfinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/ericksilva/minhasfinancas/api/resource/LancamentoResource.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ericksilva.minhasfinancas.api.dto.AtualizaStatusDTO;
 import com.ericksilva.minhasfinancas.api.dto.LancamentoDTO;
 import com.ericksilva.minhasfinancas.exceptions.RegraNegocioException;
 import com.ericksilva.minhasfinancas.model.entity.Lancamento;
@@ -59,6 +60,23 @@ public class LancamentoResource {
 				return ResponseEntity.badRequest().body(e.getMessage());
 			}
 		}).orElseGet( () -> new ResponseEntity("Lançamento não atualizado", HttpStatus.BAD_REQUEST));
+	}
+	
+	@PutMapping("{id}/atualiza-status")
+	public ResponseEntity atualizarStatus( @PathVariable("id") Long id, @RequestBody AtualizaStatusDTO dto) {
+		return service.obterPorId(id).map( entity -> {
+			StatusLancamento statusSelecionado = StatusLancamento.valueOf(dto.getStatus());
+			if(statusSelecionado == null) {
+				return ResponseEntity.badRequest().body("Não foi possivel atualizar o status de lançamento");
+			}
+			try {
+			entity.setLancamento(statusSelecionado);
+			service.atualizar(entity);
+			return ResponseEntity.ok(entity);
+			} catch (RegraNegocioException e) {
+				return ResponseEntity.badRequest().body(e.getMessage());
+			}
+		}).orElseGet( () -> new ResponseEntity("Lançamento não encontrado", HttpStatus.BAD_REQUEST));
 	}
 	
 	@DeleteMapping("{id}")
